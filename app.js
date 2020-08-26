f04481f4fee0b310a1698bcaa984abcf9ac24824
arriee <arriecodes@gmail.com>
2020-08-26 16:10:07 +0300

    v2

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const request = require('request');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const url = 'http://api.weatherstack.com/current?access_key=e9852d82c9bfdb2b2d8368d4db87b014&query=37,-122';
+const address = process.argv[2];
 
-request({url: url, json: true}, (error, response) => {
-    console.log('It\'s ' + response.body.current.temperature + 'C outside');
-})
+if (!address) {
+    console.log('Provide city name')
+} else {
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return console.log('ERROR', error);
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log('ERROR', error);
+            }
+            console.log(location);
+            console.log(forecastData);
+        })
+    })
+}
